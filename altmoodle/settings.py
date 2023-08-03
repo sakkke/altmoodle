6646f585dc0dac4f03ec853a0e5234639a4ccef4
sakkke <w32w64@gmail.com>
2023-08-03 10:47:14 +0900

    :sparkles: add dotenv support

--- altmoodle/settings.py
+++ altmoodle/settings.py
@@ -10,20 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# python-dotenv
+
+dotenv_path = os.path.join(BASE_DIR, '.env')
+
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kt(jp9qk58u-=!(f!4nera3t03!$jp4swm04-v&lqlb%z%v*xj'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-kt(jp9qk58u-=!(f!4nera3t03!$jp4swm04-v&lqlb%z%v*xj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
