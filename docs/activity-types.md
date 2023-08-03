f0813fa1ea54d758df6c4ae2786666bed0b97bf7
sakkke <w32w64@gmail.com>
2023-08-03 15:49:30 +0900

    :sparkles: add assign and unknown type

---
+++ docs/activity-types.md
@@ -0,0 +1,6 @@
+## Activity Types
+
+ID | Name the on page | Image | Fields
+--- | --- | --- | ---
+assign | 課題 | ![](https://imgur.com/Ty2M7FA.jpg) | text
+unknown | none | none | text
--- moodle_bridge/views.py
+++ moodle_bridge/views.py
@@ -2,6 +2,7 @@ import os
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from .utils.moodle import login
@@ -21,12 +22,39 @@ class Sections(APIView):
         driver.get(os.getenv('MOODLE_URL') + f'/course/view.php?id={course_id}')
 
         titles = driver.find_elements(By.XPATH, '//h3[@data-for="section_title"]')
+        sections = driver.find_elements(By.XPATH, '//li[@data-for="section"]')
 
         response = []
 
-        for i in range(len(titles)):
+        for i, section in enumerate(sections):
+            activities = []
+
+            for j, activity in enumerate(section.find_elements(By.XPATH, './/li[contains(@class, "activity")]'), 1):
+                # assign
+
+                try:
+                    section.find_element(By.XPATH, f'.//li[position() = {j} and contains(@class, "activity") and contains(@class, "modtype_assign")]')
+
+                    activities.append({
+                        'type': 'assign',
+                        'text': activity.find_element(By.XPATH, './/span[contains(@class, "instancename")]').text,
+                    })
+
+                    continue
+                except NoSuchElementException:
+                    pass
+
+
+                # unknown
+
+                activities.append({
+                    'type': 'unknown',
+                    'text': activity.find_element(By.XPATH, './/span[contains(@class, "instancename")]').text,
+                })
+
             section = {
                 'title': titles[i].text,
+                'activities': activities,
             }
 
             response.append(section)
